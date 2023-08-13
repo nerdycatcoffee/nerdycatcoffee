3630c382c230bcc16ff64fd664bfda867da23071
jrazmi <joshua@gpsimpact.com>
2023-08-13 10:00:42 -0700

    updating bg img

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ const port = process.env.PORT || 3081
 const path = require('path');
 
 
-app.use('/images',express.static(path.join(__dirname, '/files/images')));
-app.use('/js',express.static(path.join(__dirname, '/files/js')));
-app.use('/css',express.static(path.join(__dirname, '/files/css')));
-app.use('/static',express.static(path.join(__dirname, '/files/static')));
+app.use('/images',express.static(path.join(__dirname, '/src/images')));
+app.use('/js',express.static(path.join(__dirname, '/src/js')));
+app.use('/css',express.static(path.join(__dirname, '/src/css')));
+app.use('/static',express.static(path.join(__dirname, '/src/static')));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/files/index.html');
+  res.sendFile(__dirname + '/src/index.html');
 })
 
 
 app.get('/privacy.html', (req, res) => {
-    res.sendFile(__dirname + '/files/privacy.html');
+    res.sendFile(__dirname + '/src/privacy.html');
 })
 app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/files/index.html');
+    res.sendFile(__dirname + '/src/index.html');
 })
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
